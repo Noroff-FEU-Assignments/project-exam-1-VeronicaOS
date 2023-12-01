af486943984c3d1cd5e6463f4ee572183fab1810
VeronicaOS <verols51029@stud.noroff.no>
2023-12-01 23:25:43 +0100

    15

--- JS/home.js
+++ JS/home.js
@@ -1,42 +1,59 @@
 import fetchPosts from "./fetchPosts.js";
 import recipePosts from "./recipePost.js";
 
+
 async function loadPosts() {
-    // const productContainer = document.querySelector(".latestPosts"); // Corrected selector
-    const nxtBtn = document.querySelector(".next");
-    const backBtn = document.querySelector(".prev");
-    const recipeContainer = document.querySelector(".posts");
-    const popularContainer = document.querySelector(".most-popular");
-    const weekBtn = document.querySelector(".week-btn");
+    const resultsContainer = document.querySelector(".results");
+    function displayError(message = "Unknown error") {
+        return `<div class="error-msg">${message}</div>`;
+    }
+    try {
+        const nxtBtn = document.querySelector(".next");
+        const backBtn = document.querySelector(".prev");
+        const recipeContainer = document.querySelector(".posts");
+        const popularContainer = document.querySelector(".most-popular");
+        const weekBtn = document.querySelector(".week-btn");
 
-    const posts = await fetchPosts();
+        const posts = await fetchPosts();
 
-    posts.sort((a, b) => {
-        return new Date(b.date.slice(0, 10)) - new Date(a.date.slice(0, 10));
-    });
+        posts.sort((a, b) => {
+            return (
+                new Date(b.date.slice(0, 10)) - new Date(a.date.slice(0, 10))
+            );
+        });
 
-    const recipeWeek = posts.find((post) => post.title.rendered === "Macarons");
-    weekBtn.href = `blog-specific.html?id=${recipeWeek.id}`;
+        const recipeWeek = posts.find(
+            (post) => post.title.rendered === "Macarons"
+        );
+        weekBtn.href = `blog-specific.html?id=${recipeWeek.id}`;
 
-    posts.forEach((post) => recipePosts(post, recipeContainer));
+        posts.forEach((post) => recipePosts(post, recipeContainer));
 
-    let position = 0;
+        let position = 0;
 
-    nxtBtn.addEventListener("click", () => {
-        if (position >= posts.length - 4) return;
-        position++;
-        recipeContainer.style.transform = `translateX(-${position * 232}px)`;
-    });
+        nxtBtn.addEventListener("click", () => {
+            if (position >= posts.length - 4) return;
+            position++;
+            recipeContainer.style.transform = `translateX(-${
+                position * 232
+            }px)`;
+        });
 
-    backBtn.addEventListener("click", () => {
-        if (position <= 0) return;
-        position--;
-        recipeContainer.style.transform = `translateX(-${position * 232}px)`;
-    });
+        backBtn.addEventListener("click", () => {
+            if (position <= 0) return;
+            position--;
+            recipeContainer.style.transform = `translateX(-${
+                position * 232
+            }px)`;
+        });
 
-    for (let i = 8; i < 12 && i < posts.length; i++) {
-        recipePosts(posts[i], popularContainer);
+        for (let i = 8; i < 12 && i < posts.length; i++) {
+            recipePosts(posts[i], popularContainer);
+        }
+    } catch (error) {
+        resultsContainer.innerHTML = displayError("AN ERROR OCCURED");
     }
 }
 
 loadPosts();
+console.log(loadPosts)
